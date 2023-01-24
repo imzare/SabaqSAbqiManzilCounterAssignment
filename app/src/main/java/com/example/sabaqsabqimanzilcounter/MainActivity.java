92eee373d9987d8f99d7528968ca494f6a0bf2be
Hassan001 <bitf19a001@pucit.edu.pk>
2023-01-24 23:24:46 +0500

    added the ability to insert New students into listview from frontend

--- app/src/main/java/com/example/sabaqsabqimanzilcounter/MainActivity.java
+++ app/src/main/java/com/example/sabaqsabqimanzilcounter/MainActivity.java
@@ -3,16 +3,21 @@ package com.example.sabaqsabqimanzilcounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 TextView hd, ft, sb;
 ListView lw;
-
+EditText ed;
+Button btn;
 DBHandler db;
 
     @Override
@@ -23,8 +28,30 @@ DBHandler db;
         ft=findViewById(R.id.textView3footer);
         sb=findViewById(R.id.textView2);
         lw=findViewById(R.id.arraylistview);
+        ed=findViewById(R.id.editTextTextPersonName2);
+        btn=findViewById(R.id.button2);
         db = new DBHandler(this);
         RefreshGrid();
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                if (ed== null ) {
+                    Toast.makeText(MainActivity.this, "Please enter valid data", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                RecordClassManipulation data = new RecordClassManipulation(ed.getText().toString(),"", "","");
+                db.insertRecord(data);
+                RefreshGrid();
+            }
+
+        });
+
+
+
     }
 
 
