8f8f1944f4e7059bda141fe7f88f0e0676bbe096
Hassan001 <bitf19a001@pucit.edu.pk>
2023-01-25 21:58:19 +0500

    Merged Recycler View work with my current assginment

--- app/src/main/java/com/example/sabaqsabqimanzilcounter/MainActivity.java
+++ app/src/main/java/com/example/sabaqsabqimanzilcounter/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.sabaqsabqimanzilcounter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +13,8 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +22,12 @@ TextView hd, ft, sb;
 ListView lw;
 EditText ed;
 Button btn;
+
+    List<RecordClassManipulation> friendsList = new ArrayList<>();
+    RecyclerView recyclerView;
+    RecyclerView.Adapter adapter;
+    RecyclerView.LayoutManager layoutManager;
+
 DBHandler db;
 
     @Override
@@ -51,6 +61,35 @@ DBHandler db;
         });
 
 
+
+        RecordClassManipulation f9 = new RecordClassManipulation("10","Rizwan","1982","Kasur");
+        RecordClassManipulation f10 = new RecordClassManipulation("11","Junaid","1977","Islamabad");
+        RecordClassManipulation f11 = new RecordClassManipulation("12","Waseem","1967","Rawalpindi");
+
+        friendsList.addAll(Arrays.asList(new RecordClassManipulation[]{f9,f10,f11}));
+
+
+
+
+
+
+
+
+        recyclerView = findViewById(R.id.recylerViewStudent);
+
+
+        recyclerView.setHasFixedSize(true);
+
+        //LinearLayoutManager GridLayoutManager
+        layoutManager = new LinearLayoutManager(MainActivity.this);
+//        layoutManager = new LinearLayoutManager(MainActivity.this,
+//                LinearLayoutManager.HORIZONTAL,
+//                true);
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter = new theRecyclerViewAdapter(friendsList) ;
+        recyclerView.setAdapter(adapter);
+        //adapter.notifyDataSetChanged();
 
     }
 
